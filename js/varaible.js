f96ab9c239433aa117f6fe1e7087181e49cb43ac
Sagar <dev.sagartmg@gmail.com>
2022-12-26 15:59:23 +0545

    variable

---
+++ js/varaible.js
@@ -0,0 +1,125 @@
+// console.log("Hello wlordl !")
+
+
+
+/* 
+    variables
+        - contianer to store values 
+    
+    css
+    --primary-color: 
+    var(--primary)
+
+
+*/
+
+
+name = "John"
+console.log(name)
+
+/* 
+    varaible declaration
+    let <variable_name>
+    eg let age; 
+
+    age = 12 // intialization
+
+
+    let status = true // both declaration and initialziation
+
+*/
+
+let full_name = "John Doe"
+// let names = "John Doe" "Ram"
+
+var status = true
+var status = false
+console.log(full_name)
+
+let marriage_status = false // snake case
+let marriageStatus = false // camelCase  
+
+// var for = "test"  // reserved keywords 
+
+/* 
+    var - infinite declartion and re-initialziaton
+    let  -only one declartion  and  infinite re-initialziaton
+*/
+
+let age = 12
+age = 13
+
+const PI = 3.14
+// PI = 3.14
+console.log(PI)
+
+/* 
+    Data type
+        primitive data types
+            String  // text 
+            Number // number 
+                - Int 10
+                - Dobule/float  10.12
+            Boolean  
+                true 
+                false 
+            null
+            undefined
+
+        non primite  data types  collections 
+            - Array  []
+            - Object { }
+
+
+*/
+
+let bank_balance = null
+
+console.log(bank_balance)
+
+let course; // declaring course var9iable
+// course = "mERN"
+
+console.log(course);
+
+
+/*
+ array
+    collection of diferent data types 
+    usually of similar data types
+ */
+
+let fruits = ["apple", "banana"]
+
+let users = [
+    {
+        id: 1,
+        role: "b"
+    },
+    {
+        id: 2,
+        role: "s"
+    }
+]
+
+
+let names = ["ram", "shyam", full_name, {}]
+console.log(names)
+
+let person = {
+    //  KEY or ATTRIBUTE or PROPERTY : VALUE
+    name: "ram",
+    age: 12
+}
+
+console.log(person);
+
+
+
+
+
+
+
+
+
+

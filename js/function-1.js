714769d279d277b7f11677a903ac226881717d41
Sagar <dev.sagartmg@gmail.com>
2022-12-28 15:58:39 +0545

    functions

---
+++ js/function-1.js
@@ -0,0 +1,95 @@
+
+/* 
+
+DRY principle
+donot repeat yourself 
+
+*/
+
+
+/* 
+
+    TAsk: wirte a progream calculate sum of two number 
+
+*/
+
+let num1 = 1;
+let num2 = 2;
+let sum = num1 + num2
+
+// console.log("the sum is " + sum)
+// console.log("the sum is " + (num1 + num2))
+
+
+let num3 = 3;
+let num4 = 4;
+
+// console.log("the sum  of " + num4 + " and " + num3 + " is " + (num3 + num4))
+
+
+
+/* 
+function <function_name>() {
+
+    // do your task here. 
+
+}
+ */
+
+/* 
+    function - code reuse
+    syntax
+
+    to create a funciton 
+    function <function_name>(){
+
+    }
+
+
+    to execute/call a function 
+    <function_name>()
+
+
+*/
+
+
+// function calculateSum(<parameter-1>,<parameter-2>) {
+function calculateSum(num1, num2) {
+
+    // let num1 = 1
+    // let num2 = 2
+
+    // let num1 = 4
+    // let num2 = 5
+
+    // console.log("our sum task insid efucntion  ", num1, num2)
+
+    // let num5 = 5;
+    // let num6 = 6;
+
+    // console.log("the sum  of " + num1 + " and " + num2 + " is " + (num1 + num2))
+    console.log(" " + num1 + " + " + num2 + " = " + (num1 + num2))
+
+    // 1+ 2 = 3
+
+}
+
+/* 
+    calling the function
+    executing the function
+*/
+
+// calculateSum(<arguement-1><arguement-2>)  
+calculateSum(1, 2)
+calculateSum(4, 5)
+calculateSum(5, 15)
+
+
+
+
+
+
+
+
+
+
---
+++ js/function-2.js
@@ -0,0 +1,36 @@
+
+
+
+
+
+function calculateSum(num1, num2) {
+    console.log(num1 + num2)
+    return num1 + num2
+}
+
+
+/* 
+    hoisting
+*/
+
+/*
+    fat arrow function
+    arrow function
+*/
+
+
+// let calculateSum = () => {
+//     console.log("do your task")
+// }
+
+
+
+let result = calculateSum(1, 2)
+
+let result2 = calculateSum(3, 5)
+
+console.log(result);
+console.log(result2);
+
+
+

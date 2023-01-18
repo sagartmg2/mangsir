ff9d1ee73cd77bcb76e85dbf7d34bfeaebcbfaa6
Sagar <dev.sagartmg@gmail.com>
2023-01-18 15:59:25 +0545

    show product

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
-import logo from './logo.svg';
+import Navbar from "./component/Navbar";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from "./page/Login";
+import Signup from "./page/Signup";
+import Home from "./page/Home";
 
 function App() {
   return (
     <div className="">
-      navbar
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="" element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+        </Routes>
+
+      </BrowserRouter>
+
     </div>
   );
 }
---
+++ client/src/page/Home.js
@@ -0,0 +1,79 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import NOImage from "../asset/images/no-image.jpg"
+
+/* 
+    import -> ES6 module
+    require -> common JS
+*/
+
+
+const Home = () => {
+
+    const [products, setProducts] = useState([]);
+    const [isLoading, setisLoading] = useState(true);
+
+    /*  
+        database operations
+            CRUD
+                ceate
+                read
+                update
+                delete
+        
+            HTTP methods
+                GET
+                POST
+                PUT / PATCH
+                DELETE
+    
+    */
+
+    useEffect(() => {
+        axios.get("https://ecommerce-sagartmg2.vercel.app/api/products")
+            .then(res => {
+                console.log(res);
+                setisLoading(false)
+                setProducts(res.data.data[0].data)
+
+            }).catch(err => {
+
+            })
+    }, []);
+
+
+    return (
+        <div className='row'>
+            {
+                isLoading
+                &&
+                <h1>loading.. </h1>
+
+            }
+            <input/>
+            {
+                products.map(product => {
+                    return <div className='p-2 col-md-3' key={product._id}>
+                        <div className="card"  >
+                            {/* <img src={require("../asset/images/no-image.jpg")} className="card-img-top" alt="..." /> */}
+                            {/* <img src={NOImage} className="card-img-top" alt="..." /> */}
+                            <img src={`${product.images[0] || require("../asset/images/no-image.jpg")}`}
+                                className="card-img-top" alt="..."
+                                style={{
+                                    height: "180px"
+                                }}
+                            />
+                            <div className="card-body">
+                                <h5 className="card-title">{product.name}</h5>
+                                <p className="card-text">${product.price} </p>
+                                <a href="#" className="btn btn-primary">Add To Cart</a>
+                            </div>
+                        </div>
+                    </div>
+                })
+            }
+        </div>
+    );
+}
+
+export default Home;
